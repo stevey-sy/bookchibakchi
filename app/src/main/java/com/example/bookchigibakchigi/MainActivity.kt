bb6de33fa1f6b45a3c93177e9b8c56f636c030db
sinsy8989 <sinsy89@monkey-soft.com>
2024-10-30 13:59:38 +0900

    issue # toolbar 디자인 수정

--- app/src/main/java/com/example/bookchigibakchigi/MainActivity.kt
+++ app/src/main/java/com/example/bookchigibakchigi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bookchigibakchigi
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -21,15 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         // Toolbar를 ActionBar로 설정
         setSupportActionBar(binding.toolbar)
+        // 백버튼 제거
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.setHomeButtonEnabled(false)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // AppBarConfiguration 설정
+        // AppBarConfiguration 설정 (여기서 백버튼을 비활성화할 최상위 목적지를 지정)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_my_library, R.id.navigation_pick_book, R.id.navigation_community, R.id.navigation_record, R.id.navigation_setting
+                R.id.navigation_my_library,
+                R.id.navigation_pick_book,
+                R.id.navigation_community,
+                R.id.navigation_record,
+                R.id.navigation_setting
             )
         )
 
@@ -39,15 +46,9 @@ class MainActivity : AppCompatActivity() {
         // ActionBar와 NavController 연결
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        // 초기 타이틀 설정
-        val currentDestination = navController.currentDestination
-        currentDestination?.let {
-            updateToolbarTitle(it.id)
-        }
-
         // NavController의 목적지가 변경될 때마다 Toolbar의 타이틀을 업데이트
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            updateToolbarTitle(destination.id)
+//            updateToolbarTitle(destination.id)
         }
     }
 
