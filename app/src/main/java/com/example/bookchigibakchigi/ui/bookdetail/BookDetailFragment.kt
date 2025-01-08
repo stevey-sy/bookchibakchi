2837b4c9b48ec067b7002fa4abd992193dbcce03
sinsy8989 <sinsy89@hanmail.net>
2025-01-08 14:07:27 +0900

    책 상세화면 DataBinding을 이용한 소스 간략화

--- app/src/main/java/com/example/bookchigibakchigi/ui/bookdetail/BookDetailFragment.kt
+++ app/src/main/java/com/example/bookchigibakchigi/ui/bookdetail/BookDetailFragment.kt
@@ -30,36 +30,24 @@ class BookDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBookDetailBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // AppDatabase와 BookShelfRepository 생성
+        // ViewModel 초기화
         val bookDao = AppDatabase.getDatabase(requireContext()).bookDao()
         val repository = BookShelfRepository(bookDao)
-
-        // ViewModel 초기화
         viewModel = ViewModelProvider(this, MyLibraryViewModelFactory(repository))
             .get(MyLibraryViewModel::class.java)
+
+        // ViewModel 바인딩
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         adapter = BookViewPagerAdapter()
         binding.viewPager.adapter = adapter
 
-        // Observe LiveData
-        viewModel.bookShelfItems.observe(viewLifecycleOwner) { bookList ->
-            adapter.setDataList(bookList)
-        }
+        return binding.root
+    }
 
-        // Observe 현재 선택된 책 데이터
-        viewModel.currentBook.observe(viewLifecycleOwner) { currentBook ->
-            binding.tvBookTitle.text = currentBook.title
-            binding.tvAuthor.text = currentBook.author
-            binding.tvPublisher.text = currentBook.publisher
-        }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // ViewPager2의 PageChangeCallback 설정
         binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
@@ -67,33 +55,5 @@ class BookDetailFragment : Fragment() {
                 viewModel.updateCurrentBook(position) // 선택된 페이지 업데이트
             }
         })
-
-        // 초기 진행률과 목표 진행률 설정
-        val initialProgress = 0
-        val targetProgress = 70 // 목표 진행률 (%)
-
-        // 프로그레스바 배경의 전체 길이 가져오기
-        binding.progressBarBackground.post {
-            val totalWidth = binding.progressBarBackground.width
-
-            // 애니메이션: 프로그레스바의 너비 늘리기
-            val progressBarAnimation = ValueAnimator.ofInt(initialProgress, targetProgress)
-            progressBarAnimation.duration = 1000 // 애니메이션 시간 (1초)
-            progressBarAnimation.addUpdateListener { animator ->
-                val progress = animator.animatedValue as Int
-                val progressWidth = (totalWidth * progress) / 100
-
-                // 프로그레스바 길이 업데이트
-                val params = binding.progressBarForeground.layoutParams
-                params.width = progressWidth
-                binding.progressBarForeground.layoutParams = params
-
-                // 진행 퍼센트 텍스트 위치와 내용 업데이트
-                binding.tvProgressPercentage.translationX = progressWidth.toFloat()
-                binding.tvProgressPercentage.text = "$progress%"
-            }
-            progressBarAnimation.start()
-        }
-
     }
 }
--- app/src/main/java/com/example/bookchigibakchigi/util/BindingAdapters.kt
+++ app/src/main/java/com/example/bookchigibakchigi/util/BindingAdapters.kt
@@ -1,10 +1,15 @@
 package com.example.bookchigibakchigi.util
 
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.bookchigibakchigi.R
+import com.example.bookchigibakchigi.data.entity.BookEntity
+import com.example.bookchigibakchigi.ui.bookdetail.adapter.BookViewPagerAdapter
 
 object BindingAdapters {
     @JvmStatic
@@ -16,5 +21,25 @@ object BindingAdapters {
                 RequestOptions() // 로딩 중 표시할 기본 이미지
                 .error(R.drawable.img_book_placeholder))            // 오류 시 표시할 이미지
             .into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("progressPercentage")
+    fun setProgressBarWidth(view: View, progress: Int) {
+        val parentWidth = (view.parent as ViewGroup).width // 부모 뷰의 전체 너비 가져오기
+        val newWidth = (parentWidth * progress) / 100      // 진행도에 따른 너비 계산
+        val layoutParams = view.layoutParams
+        layoutParams.width = newWidth
+        view.layoutParams = layoutParams
+    }
+
+    @JvmStatic
+    @BindingAdapter("submitBookList")
+    fun bindBookList(viewPager: ViewPager2, bookList: List<BookEntity>?) {
+        if (bookList == null) return
+        val adapter = viewPager.adapter
+        if (adapter is BookViewPagerAdapter) {
+            adapter.setDataList(bookList)
+        }
     }
 }
