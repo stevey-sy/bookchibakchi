7fae927e39e9956b3d7583834dee25d97cf86fe3
sinsy8989 <sinsy89@hanmail.net>
2025-02-02 14:53:24 +0900

    책장에 아이콘 추가

--- app/src/main/java/com/example/bookchigibakchigi/ui/mylibrary/adapter/BookShelfAdapter.kt
+++ app/src/main/java/com/example/bookchigibakchigi/ui/mylibrary/adapter/BookShelfAdapter.kt
@@ -25,8 +25,8 @@ class BookShelfAdapter(
 
     override fun getItemCount(): Int {
         // 현재 데이터 개수를 기준으로 3의 배수로 맞춤
-        val rowCount = Math.ceil(dataList.size / 3.0).toInt() // 필요한 행의 개수
-        return rowCount * 3 // 3의 배수로 반환
+        val rowCount = Math.ceil(dataList.size / 3.0).toInt() // 필요한 행의 개수 계산
+        return maxOf(9, rowCount * 3) // 최소 9개 이상 유지
     }
 
     override fun onBindViewHolder(holder: BookShelfItemViewHolder, position: Int) {
@@ -70,6 +70,8 @@ class BookShelfAdapter(
             binding.vBookShadow.visibility = View.VISIBLE
             binding.vBookShadowUp.visibility = View.VISIBLE
             binding.ivPlant.visibility = View.INVISIBLE
+            binding.ivClock.visibility = View.INVISIBLE
+            binding.ivClock.visibility = View.INVISIBLE
             if (bookEntity.coverImageUrl.isNotEmpty()) {
                 binding.ivBook.visibility = View.VISIBLE
                 Glide.with(context)
@@ -79,7 +81,6 @@ class BookShelfAdapter(
             } else {
                 // 이미지 URL이 비어 있는 경우 기본 이미지로 설정
                 binding.ivBook.visibility = View.GONE
-//                binding.rlPlus.setBackgroundResource(R.drawable.add)
             }
 
         }
@@ -92,6 +93,8 @@ class BookShelfAdapter(
             binding.vBookShadow.visibility = View.INVISIBLE
             binding.vBookShadowUp.visibility = View.INVISIBLE
             binding.ivPlant.visibility = if (position == itemCount - 1) View.VISIBLE else View.INVISIBLE
+            binding.ivClock.visibility = if (position == itemCount - 3) View.VISIBLE else View.INVISIBLE
+            binding.ivFlower.visibility = if (position == itemCount - 5) View.VISIBLE else View.INVISIBLE
 
             val context = binding.root.context
             val drawableRes = when (position % 3) {
