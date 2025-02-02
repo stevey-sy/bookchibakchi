4420fd5df8d14cca9e4016290e43dc482818ab94
sinsy <sinsy89@hanmail.net>
2025-02-02 23:46:48 +0900

    memo activity 추가 중

---
+++ app/src/main/java/com/example/bookchigibakchigi/ui/memo/MemoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.bookchigibakchigi.ui.memo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bookchigibakchigi.R
+
+class MemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_memo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
