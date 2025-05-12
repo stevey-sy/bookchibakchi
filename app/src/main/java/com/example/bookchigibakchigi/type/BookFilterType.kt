539257ed2376ff014697ed5346038558be5ccff3
stevey-sy <sinsy89@hanmail.net>
2025-05-12 10:16:54 +0900

    사용하지 않는 클래스 제거

--- app/src/main/java/com/example/bookchigibakchigi/type/BookFilterType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookchigibakchigi.type
-
-sealed class BookFilterType {
-    object Reading : BookFilterType()
-    object Finished : BookFilterType()
-    object All : BookFilterType()
-} 
--- app/src/main/java/com/example/bookchigibakchigi/type/BookStateType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bookchigibakchigi.type
-
-enum class BookStateType {
-    ALL,       // 전체
-    READING,   // 읽는 중인 책
-    FINISHED   // 다 읽은 책
-}
