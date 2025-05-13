21b929135ab971d4b923659f3ae061cd3507de24
stevey-sy <sinsy89@hanmail.net>
2025-05-13 15:53:55 +0900

    버전 수정 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,9 +19,8 @@ android {
         applicationId = "com.example.bookchigibakchigi"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-//        buildConfigField("String", "API_KEY", getApiKey("API_KEY"))
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
